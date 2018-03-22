dbd0520cc4bf480c7b542cc49bc809fdc1525156
Anton Tyutin <anton@tyutin.ru>
2018-03-22 17:56:47 +0300

    Propagate CHLD and USR1 signal to the Process Manager

--- build/run-nginx.sh
+++ build/run-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trapIt () { "$@"& pid="$!"; trap "kill -INT $pid" INT TERM; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec;};
+trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -$SGNL $pid" "$SGNL"; done; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec; };
 
 STATIC=/var/www/
 args=" $@ "
