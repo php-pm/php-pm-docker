1dee6445bf16232f3031e097535c25b9996e2f1d
Anton Tyutin <tiutin@yandex.ru>
2020-01-23 09:13:43 +0300

    Propagate CHLD and USR1 signal to the Process Manager (#11)

--- build/run-ppm.sh
+++ build/run-ppm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-trapIt () { "$@"& pid="$!"; trap "kill -INT $pid" INT TERM; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec;};
+trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -$SGNL $pid" "$SGNL"; done; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec; };
 
 trapIt /ppm/vendor/bin/ppm --ansi "$@"
--- build/run-standalone.sh
+++ build/run-standalone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trapIt () { "$@"& pid="$!"; trap "kill -INT $pid" INT TERM; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec;};
+trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -$SGNL $pid" "$SGNL"; done; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec; };
 
 ARGS='--port 80'
 trapIt /ppm/vendor/bin/ppm start --ansi $ARGS  $@
